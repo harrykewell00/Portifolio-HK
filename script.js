75fb0cc73fce62268977af5b6dfd5a875799beed
kiwdazueira69 <harrykewell2310@gmail.com>
2025-07-26 16:23:10 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// Animação suave ao rolar
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    const section = document.querySelector(this.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
